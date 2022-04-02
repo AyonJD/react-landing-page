8ba6dd05c96385ddb4d2ce912be39c4615ea0bed
Ayon <ayonjodder177@gmail.com>
2022-04-02 18:36:41 +0600

    Home top left done

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WinTheWorld from './WinTheWorld/WinTheWorld';
+
+const Home = () => {
+    return (
+        <div>
+            <WinTheWorld></WinTheWorld>
+        </div>
+    );
+};
+
+export default Home;
--- src/Components/Home/WinTheWorld/WinTheWorld.js
+++ src/Components/Home/WinTheWorld/WinTheWorld.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BsArrowUpRight } from 'react-icons/bs'
+import './WinTheWorld.css'
 
 const WinTheWorld = () => {
     return (
         <div>
-            
+            <div className="win-the-world flex">
+                <div className="left">
+                    <h1 className='speak-to-win'><span className='custom-color'>Speak</span> to<br/> win the<br/> world</h1>
+                    <p className='speak-to-win-text'>A word can change your life & peresonality in the world, in this course can can find everything.</p>
+                    <div className="button">
+                        <p>Get Started Now</p>
+                        <BsArrowUpRight></BsArrowUpRight>
+                    </div>
+                </div>
+                <div className="right">
+                    <img src="images/Pattern.svg" alt="" />
+                </div>
+            </div>
+            <div className="counter"></div>
         </div>
     );
 };
